3c5e7ece3057b817ace41b212373e6e4330b3e06
Christian Lara <christian.lara.091204@gmail.com>
2024-10-24 10:47:29 -0600

    add more files

--- def.py
+++ def.py
@@ -8,10 +8,12 @@ def es_par(num):
 print(es_par(2))
 
 
+#Par o no
 def es_paar(num):
     return (num % 2) == 0
 print(es_paar(2))
 
+# Palindrome 1
 def es_palindrome (palabra):
     for i in palabra:
         if palabra == palabra[::-1]:
@@ -20,6 +22,7 @@ def es_palindrome (palabra):
             return False
 print(es_palindrome("reconocer"))
 
+#Palindorme 2
 def es_palindrome2(palabra):
     temp = ''
     for i in range (len(palabra)):
@@ -30,6 +33,7 @@ def es_palindrome2(palabra):
         return False
 print(es_palindrome2("saul"))
 
+#Palindorme 3
 def es_palindrome3(palabra):
     if len(palabra) == 0 or len(palabra) == 1:
         return True
@@ -37,3 +41,11 @@ def es_palindrome3(palabra):
         return palabra[0] == palabra[-1] and es_palindrome3(palabra[1:-1])
     return False
 print(es_palindrome3("radar"))
+
+# Factorial
+def factorial(num):
+    resultado = 1
+    for n in range (num):
+        resultado *= n+1
+    return resultado
+print(factorial(5))
